348ee5fd37c9cef0e18d18e0d169993019bcdfa7
jtorrex <jtorrex@posteo.net>
2023-03-15 20:06:59 +0100

    Added init scripts

---
+++ .local/bin/init/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+~/.local/bin/init/per-session-init.sh
+~/.local/bin/init/work-session-init.sh
+~/.local/bin/init/tmux-session-init.sh
---
+++ .local/bin/init/per-session-init.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+echo "Adding ssh keys..."
+ssh-add ~/.ssh/ed25519-torrex-key
+ssh-add ~/.ssh/provisioning-homelab
