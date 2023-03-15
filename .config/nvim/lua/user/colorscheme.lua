d470bfc949288263778329b06a007e4c5c0d4383
jtorrex <jtorrex@posteo.net>
2023-03-15 21:22:43 +0100

    * Added new configuration

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -4,3 +4,19 @@ local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
 if not status_ok then
   return
 end
+
+return {
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+
+  -- catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+  },
+}
--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -7,21 +7,11 @@ local opts = { silent = true }
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 
--- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
-
 -- Normal --
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-
+keymap("n", "<C-k>", "<C-w>k", opts) keymap("n", "<C-l>", "<C-w>l", opts)
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
@@ -55,29 +45,37 @@ keymap("v", ">", ">gv", opts)
 -- NvimTree
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
--- Telescope
-keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
-keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
-keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
-
--- Git
-keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
-
--- Comment
-keymap("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
-keymap("x", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", opts)
-
--- DAP
-keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
-keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", opts)
-keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", opts)
-keymap("n", "<leader>do", "<cmd>lua require'dap'.step_over()<cr>", opts)
-keymap("n", "<leader>dO", "<cmd>lua require'dap'.step_out()<cr>", opts)
-keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
-keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
-keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
-keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
-
--- Lsp
-keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
+-- " Mapping keys settings
+-- " Hold Ctrl and use keys to move around splits
+-- map <C-k> <C-w><Up>
+-- map <C-j> <C-w><Down>
+-- map <C-l> <C-w><Right>
+-- map <C-h> <C-w><Left>
+-- " Create a new tab
+-- map <C-t> :tabnew<cr>
+-- " Ctrl-tab to switch buffers
+-- map <C-Tab> :bnext<cr>
+-- map <C-S-Tab> :bprevious<cr>
+-- map <C-Pageup> :tabprev <cr>
+-- map <C-Pagedown> :tabnext <cr>
+-- map <C-w>- :new <cr>
+-- map <C-w>_ :vnew <cr>
+-- map <leader>- :split<cr>
+-- 
+-- " Open new split panes to right and bottom. More natural
+-- set splitbelow
+-- set splitright
+-- 
+-- " session management
+-- nnoremap <leader>so :OpenSession<Space>
+-- nnoremap <leader>ss :SaveSession<Space>
+-- nnoremap <leader>sd :DeleteSession<CR>
+-- nnoremap <leader>sc :CloseSession<CR>
+-- 
+-- map <leader>_ :vsplit<cr>
+-- " Do not enter ex mode when I fat finger q with shift pressed
+-- nnoremap Q <nop>
+-- " Do not show me man pages when I'm bad at pressing k
+-- nnoremap K <nop>
+-- " Turn off recording
+-- map q <Nop>
