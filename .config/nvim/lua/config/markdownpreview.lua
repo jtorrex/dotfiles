ce5bd932f1545a738ff916a7331ac8ae7c0f0826
jtorrex <jtorrex@posteo.net>
2023-11-25 17:20:47 +0100

    Working config for Markdown Preview

--- .config/nvim/lua/config/markdownpreview.lua
+++ .config/nvim/lua/config/markdownpreview.lua
@@ -1,33 +1,32 @@
 vim.cmd([[let g:mkdp_auto_start = 1]])
--- vim.cmd([[let g:mkdp_auto_close = 1]])
--- vim.cmd([[let g:mkdp_refresh_slow = 0]])
--- vim.cmd([[let g:mkdp_command_for_global = 0]])
--- vim.cmd([[let g:mkdp_open_to_the_world = 0]])
--- vim.cmd([[let g:mkdp_open_ip = '']])
--- vim.cmd([[let g:mkdp_browser = '']])
--- vim.cmd([[let g:mkdp_echo_preview_url = 0]])
--- vim.cmd([[let g:mkdp_browserfunc = '']])
--- vim.cmd([[let g:mkdp_preview_options = {
---     \ 'mkit': {},
---     \ 'katex': {},
---     \ 'uml': {},
---     \ 'maid': {},
---     \ 'disable_sync_scroll': 0,
---     \ 'sync_scroll_type': 'middle',
---     \ 'hide_yaml_meta': 1,
---     \ 'sequence_diagrams': {},
---     \ 'flowchart_diagrams': {},
---     \ 'content_editable': v:false,
---     \ 'disable_filename': 0,
---     \ 'toc': {}
---     \ }]])
--- vim.cmd([[let g:mkdp_markdown_css = '']])
--- vim.cmd([[let g:mkdp_highlight_css = '']])
--- vim.cmd([[let g:mkdp_port = '']])
--- vim.cmd([[let g:mkdp_page_title = '「${name}」']])
--- -- vim.cmd([[let g:mkdp_images_path = /home/jtorrex/.markdown_images]])
--- vim.cmd([[let g:mkdp_filetypes = 'markdown']])
--- vim.cmd([[let g:mkdp_theme = 'dark']])
--- vim.cmd([[let g:mkdp_combine_preview = 0]])
--- vim.cmd([[let g:mkdp_combine_preview = 0]])
--- vim.cmd([[let g:mkdp_combine_preview_auto_refresh = 1]])
+vim.cmd([[let g:mkdp_auto_close = 0]])
+vim.cmd([[let g:mkdp_refresh_slow = 0]])
+vim.cmd([[let g:mkdp_command_for_global = 0]])
+vim.cmd([[let g:mkdp_open_to_the_world = 0]])
+vim.cmd([[let g:mkdp_open_ip = '']])
+vim.cmd([[let g:mkdp_browser = '']])
+vim.cmd([[let g:mkdp_echo_preview_url = 0]])
+vim.cmd([[let g:mkdp_browserfunc = '']])
+vim.cmd([[let g:mkdp_preview_options = {
+    \ 'mkit': {},
+    \ 'katex': {},
+    \ 'uml': {},
+    \ 'maid': {},
+    \ 'disable_sync_scroll': 0,
+    \ 'sync_scroll_type': 'middle',
+    \ 'hide_yaml_meta': 1,
+    \ 'sequence_diagrams': {},
+    \ 'flowchart_diagrams': {},
+    \ 'content_editable': v:false,
+    \ 'disable_filename': 0,
+    \ 'toc': {}
+    \ }]])
+vim.cmd([[let g:mkdp_markdown_css = '']])
+vim.cmd([[let g:mkdp_highlight_css = '']])
+vim.cmd([[let g:mkdp_port = '']])
+vim.cmd([[let g:mkdp_page_title = '「${name}」']])
+-- vim.cmd([[let g:mkdp_images_path = /home/jtorrex/.markdown_images]])
+vim.cmd([[let g:mkdp_filetypes = 'markdown']])
+-- vim.cmd([[let g:mkdp_theme = 'light']])
+vim.cmd([[let g:mkdp_combine_preview = 1]])
+vim.cmd([[let g:mkdp_combine_preview_auto_refresh = 1]])
