80d8533a3b8303a1d901110a0b56eaeeaca6fb92
jtorrex <jtorrex@posteo.net>
2024-02-05 10:51:25 +0100

    nvim: fixing markdownpreview plugin

--- .config/nvim/lua/core/markdownpreview.lua
+++ .config/nvim/lua/core/markdownpreview.lua
@@ -1,13 +1,14 @@
-vim.cmd([[let g:mkdp_auto_start = 1]])
-vim.cmd([[let g:mkdp_auto_close = 0]])
-vim.cmd([[let g:mkdp_refresh_slow = 0]])
-vim.cmd([[let g:mkdp_command_for_global = 0]])
-vim.cmd([[let g:mkdp_open_to_the_world = 0]])
-vim.cmd([[let g:mkdp_open_ip = '']])
-vim.cmd([[let g:mkdp_browser = '']])
-vim.cmd([[let g:mkdp_echo_preview_url = 0]])
-vim.cmd([[let g:mkdp_browserfunc = '']])
-vim.cmd([[let g:mkdp_preview_options = {
+vim.cmd [[
+  let g:mkdp_auto_start = 1
+  let g:mkdp_auto_close = 0
+  let g:mkdp_refresh_slow = 0
+  let g:mkdp_command_for_global = 0
+  let g:mkdp_open_to_the_world = 0
+  let g:mkdp_open_ip = ''
+  let g:mkdp_browser = ''
+  let g:mkdp_echo_preview_url = 0
+  let g:mkdp_browserfunc = ''
+  let g:mkdp_preview_options = {
     \ 'mkit': {},
     \ 'katex': {},
     \ 'uml': {},
@@ -20,13 +21,12 @@ vim.cmd([[let g:mkdp_preview_options = {
     \ 'content_editable': v:false,
     \ 'disable_filename': 0,
     \ 'toc': {}
-    \ }]])
-vim.cmd([[let g:mkdp_markdown_css = '']])
-vim.cmd([[let g:mkdp_highlight_css = '']])
-vim.cmd([[let g:mkdp_port = '']])
-vim.cmd([[let g:mkdp_page_title = '「${name}」']])
--- vim.cmd([[let g:mkdp_images_path = /home/jtorrex/.markdown_images]])
-vim.cmd([[let g:mkdp_filetypes = 'markdown']])
--- vim.cmd([[let g:mkdp_theme = 'light']])
-vim.cmd([[let g:mkdp_combine_preview = 1]])
-vim.cmd([[let g:mkdp_combine_preview_auto_refresh = 1]])
+    \ }
+  let g:mkdp_markdown_css = ''
+  let g:mkdp_highlight_css = ''
+  let g:mkdp_port = ''
+  let g:mkdp_page_title = '${name}」'
+  let g:mkdp_filetypes = 'markdown'
+  let g:mkdp_combine_preview = 1
+  let g:mkdp_combine_preview_auto_refresh = 1
+]]
