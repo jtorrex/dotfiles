ae7c5df33c932ead9ac6fdb8a31a1adc08414789
jtorrex <jtorrex@posteo.net>
2024-02-05 10:51:38 +0100

    nvim: modified keymaps for Telescope

--- .config/nvim/lua/core/telescope.lua
+++ .config/nvim/lua/core/telescope.lua
@@ -1,5 +1,8 @@
 -- Telescope
-vim.cmd([[noremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>]])
-vim.cmd([[noremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>]])
-vim.cmd([[noremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>]])
-vim.cmd([[noremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>]])
+require('telescope').setup({ file_ignore_patterns = { "node%_modules/.*" } })
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fg', builtin.live_grep, {})
+vim.keymap.set('n', 'fb', builtin.buffers, {})
+vim.keymap.set('n', 'fh', builtin.help_tags, {})
