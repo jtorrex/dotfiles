29988abfcb478d3f82527b5d8e15631dfd4e7010
jtorrex <jtorrex@posteo.net>
2024-06-21 15:31:17 +0200

    tmux sessions: modified splits on tmux

--- .local/bin/init/tmux-session-init.sh
+++ .local/bin/init/tmux-session-init.sh
@@ -2,11 +2,13 @@
 PER_SESSION="per"
 DEV_SESSION="dev"
 WIKI_SESSION="wiki"
+WORK_SESSION="work"
 
 echo "Starting tmux configuration..."
 PER_SESSIONEXISTS=$(tmux list-sessions | grep $PER_SESSION)
 DEV_SESSIONEXISTS=$(tmux list-sessions | grep $DEV_SESSION)
 WIKI_SESSIONEXISTS=$(tmux list-sessions | grep $WIKI_SESSION)
+WORK_SESSIONEXISTS=$(tmux list-sessions | grep $WORK_SESSION)
 
 # Only create tmux session if it doesn't already exist if [ "$PER_SESSIONEXISTS" = "" ] then
 if [ "$PER_SESSIONEXISTS" = "" ]
@@ -15,24 +17,24 @@ then
 
     tmux new-session -d -s $PER_SESSION
     tmux send-keys 'clear && neomutt' C-m
-    tmux split-window -hf
-    tmux send-keys 'ikhal' C-m # Switch to bind script?
-    tmux split-window
-    tmux send-keys 'todo list' C-m # Switch to bind script
 
     tmux new-window -t $PER_SESSION:2
-    tmux send-keys 'ranger' C-m # Switch to bind script?
-    tmux split-window -hf
-    tmux send-keys 'ncmpcpp' C-m # Switch to bind script?
-    tmux split-window
-    tmux send-keys 'newsboat' C-m # Switch to bind script?
+    tmux send-keys 'ikhal' C-m
 
     tmux new-window -t $PER_SESSION:3
-    tmux send-keys 'btop' C-m # Switch to bind script?
-    tmux split-window -hf
-    tmux send-keys 'ls' C-m # Switch to bind script?
-    tmux split-window
-    tmux send-keys 'netstat' C-m # Switch to bind script?
+    tmux send-keys 'todo list' C-m
+
+    tmux new-window -t $PER_SESSION:4
+    tmux send-keys 'ranger' C-m 
+
+    tmux new-window -t $PER_SESSION:5
+    tmux send-keys 'ncmpcpp' C-m
+
+    tmux new-window -t $PER_SESSION:6
+    tmux send-keys 'newsboat' C-m
+
+    tmux new-window -t $PER_SESSION:7
+    tmux send-keys 'btop' C-m
 fi
 
 if [ "$DEV_SESSIONEXISTS" = "" ]
@@ -41,9 +43,9 @@ then
 
     tmux new-session -d -s $DEV_SESSION
     tmux send-keys 'clear' C-m
-    tmux split-window -hf
-    tmux send-keys 'clear' C-m # Switch to bind script?
-    tmux split-window
+    tmux new-window -t $DEV_SESSION:2
+    tmux send-keys 'clear' C-m
+    tmux new-window -t $DEV_SESSION:3
     tmux send-keys 'clear' C-m
 fi
 
@@ -53,7 +55,19 @@ then
     echo "Creating new $WIKI_SESSION"
 
     tmux new-session -d -s $WIKI_SESSION
-    tmux send-keys 'cd ~/Sync/wiki && vim index.md' C-m # Switch to bind script?
+    tmux send-keys 'cd ~/Sync/wiki && vim index.md' C-m
+fi
+
+if [ "$WORK_SESSIONEXISTS" = "" ]
+then
+    echo "Creating new $WORK_SESSION"
+
+    tmux new-session -d -s $WORK_SESSION
+    tmux send-keys 'clear' C-m
+    tmux new-window -t $WORK_SESSION:2
+    tmux send-keys 'clear' C-m
+    tmux new-window -t $WORK_SESSION:3
+    tmux send-keys 'clear' C-m
 fi
 
 echo "Starting tmux configuration... [DONE]"
