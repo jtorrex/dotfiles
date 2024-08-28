3a57aed41a8938ec6d556f5e121d279dcb060a34
jtorrex <jtorrex@posteo.net>
2024-08-28 15:54:19 +0200

    nvim: added latest changes about completions

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -10,12 +10,32 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities
 require("lspconfig").clangd.setup {
   capabilities = capabilities
 }
+
 require("lspconfig").lua_ls.setup {
   capabilities = capabilities
 }
-require("lspconfig").yamlls.setup {
-  capabilities = capabilities
+
+-- https://www.arthurkoziel.com/json-schemas-in-neovim/
+require('lspconfig').yamlls.setup {
+  settings = {
+    yaml = {
+      validate = true,
+      -- disable the schema store
+      schemaStore = {
+        enable = false,
+        url = "",
+      },
+      -- manually select schemas
+      schemas = {
+        ['https://json.schemastore.org/kustomization.json'] = 'kustomization.{yml,yaml}',
+        ['https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json'] = 'docker-compose*.{yml,yaml}',
+        ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json"] = "argocd-application.yaml",
+        kubernetes = { 'k8s**.yaml', 'kube*/*.yaml' }
+      }
+    }
+  }
 }
+
 require("lspconfig").helm_ls.setup {
   capabilities = capabilities
 }
