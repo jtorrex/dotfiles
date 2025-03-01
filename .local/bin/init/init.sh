2cadf3629807c2e31364e6ef493a27acc9ace427
jtorrex <jtorrex@posteo.net>
2025-03-01 17:06:09 +0100

    Removed scripts from init dir

--- .local/bin/init/init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-~/.local/bin/init/ssh-keys-init.sh
-~/.local/bin/init/tmux-session-init.sh
--- .local/bin/init/ssh-keys-init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-eval $(keychain --eval ~/.ssh/ed25519-torrex-key)
-eval $(keychain --eval ~/.ssh/hpcnow-jtorres-key)
-eval $(keychain --eval ~/.ssh/provisioning-homelab)
