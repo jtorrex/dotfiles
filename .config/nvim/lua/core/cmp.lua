13f1cdedeadf95c3a48ebf5232567a4dc9e646c7
jtorrex <jtorrex@posteo.net>
2025-04-20 03:09:49 +0200

    renamed to cmp and added extra options

---
+++ .config/nvim/lua/core/cmp.lua
@@ -0,0 +1,54 @@
+-- Set up CMP
+-- https://github.com/hrsh7th/nvim-cmp
+local cmp = require ("cmp")
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+
+  -- REQUIRED - you must specify a snippet engine
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), --Accept current
+  }),
+
+  -- Sources for snippets
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip'},
+    { name = 'buffer' }
+  }),
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  }),
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+  }),
+})
