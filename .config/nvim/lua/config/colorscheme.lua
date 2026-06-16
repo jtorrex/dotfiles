e0b64e516a4d5f9e2c24f5a93ac1a691f0ebce35
Jose E Torres <jtorrex@posteo.net>
2026-06-16 10:41:13 +0200

    all: Custom Catppuccin Theme + font for various tools

--- .config/nvim/lua/config/colorscheme.lua
+++ .config/nvim/lua/config/colorscheme.lua
@@ -1,7 +1,7 @@
 -- Background
 -- vim.o.background = "dark"
 -- vim.o.background = "light"
-vim.cmd([[colorscheme tokyonight-night]])
+vim.cmd([[colorscheme catppuccin-mocha]])
 -- vim.cmd([[colorscheme gruvbox]])
 -- vim.cmd([[colorscheme papercolor-theme]])
 
