31ce2bcc3db72fdeddeed79d69f95ce6cab690c4
Jose E Torres <jtorrex@posteo.net>
2026-06-16 10:45:28 +0200

    link: Fix dots linkage

--- link_dots.sh
+++ link_dots.sh
@@ -1,115 +1,34 @@
 #!/bin/bash
 echo "Configuring dotfiles"
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Configure Xresources"
-rm -rf ~/.xresources
-rm -rf ~/.Xresources
-ln -s $(echo $PWD)/.xresources ~/.xresources
+rm -rf ~/.xresources ~/.Xresources
+ln -sf "$DOTFILES_DIR/.xresources" ~/.xresources
 
 echo "Configure xinitrc"
 rm -rf ~/.xinitrc
-ln -s $(echo $PWD)/.xinitrc ~/.xinitrc
+ln -sf "$DOTFILES_DIR/.xinitrc" ~/.xinitrc
 
-echo "Configure autorandr"
-rm -rf ~/.config/autorandr
-ln -s $(echo $PWD)/.config/autorandr ~/.config/
-
-echo "Configure VIM"
-rm -rf ~/.config/nvim
-ln -s $(echo $PWD)/.config/nvim ~/.config/nvim
-
-echo "Configure tmux"
-rm -rf ~/.config/tmux
-ln -s $(echo $PWD)/.config/tmux ~/.config/tmux
-
-echo "Configure zshrc"
-rm -rf ~/.zshrc
-ln -s $(echo $PWD)/.zshrc ~/.zshrc
+echo "Configure XDG configs"
+for dir in alacritty btop cointop compton dunst fish foot ghostty git gtk-3.0 i3 i3status i3status-rust kitty mako mpd mpv ncmpcpp newsboat nnn nvim opencode pcmanfm-qt polybar ranger redshift ripgrep rofi starship swappy sway swaylock tmux waybar wdisplays yay yazi zathura zsh; do
+    if [ -d "$DOTFILES_DIR/.config/$dir" ]; then
+        echo "  -> .config/$dir"
+        rm -rf ~/.config/$dir
+        ln -sf "$DOTFILES_DIR/.config/$dir" ~/.config/
+    fi
+done
 
 echo "Configure zshenv"
-rm -rf ~/.zshenv
-ln -s $(echo $PWD)/.zshenv ~/
-
-echo "Configure aliases"
-rm -rf ~/.aliases
-ln -s $(echo $PWD)/.aliases ~/
-
-echo "Configure i3"
-rm -rf ~/.config/i3
-ln -s $(echo $PWD)/.config/i3 ~/.config/
-
-echo "Configure i3blocks"
-rm -rf ~/.config/i3blocks
-ln -s $(echo $PWD)/.config/i3blocks ~/.config/
-
-echo "Configure i3status-rust"
-rm -rf ~/.config/i3status-rust
-ln -s $(echo $PWD)/.config/i3status-rust ~/.config/
-
-echo "Configure rofi"
-rm -rf ~/.config/rofi
-ln -s $(echo $PWD)/.config/rofi ~/.config/
-
-echo "Configure ranger"
-rm -rf ~/.config/ranger
-ln -s $(echo $PWD)/.config/ranger ~/.config/
-
-echo "Configure compton"
-rm -rf ~/.config/compton
-ln -s $(echo $PWD)/.config/compton ~/.config/
-
-echo "Configure alacritty"
-rm -rf ~/.config/alacritty
-ln -s $(echo $PWD)/.config/alacritty ~/.config/
-
-echo "Configure polybar"
-rm -rf ~/.config/polybar
-ln -s $(echo $PWD)/.config/polybar ~/.config/
-
-echo "Configure themes"
-rm -rf ~/.themes
-ln -s $(echo $PWD)/.themes ~/.themes
-
-echo "Configure irssi"
-rm -rf ~/.irssi
-ln -s $(echo $PWD)/.irssi ~/.irssi
-
-echo "Configure mpd"
-rm -rf ~/.config/mpd
-ln -s $(echo $PWD)/.config/mpd ~/.config/
-
-echo "Configure go"
-rm -rf ~/.config/go
-ln -s $(echo $PWD)/.config/mpd ~/.config/
+rm -f ~/.zshenv
+ln -sf "$DOTFILES_DIR/.config/zsh/zshenv" ~/.zshenv
 
 echo "Configure .local/bin"
 rm -rf ~/.local/bin
-ln -s $(echo $PWD)/.local/bin ~/.local/bin
+ln -sf "$DOTFILES_DIR/.local/bin" ~/.local/
 
-echo "Configure newsboat"
-rm -rf ~/.config/newsboat
-ln -s $(echo $PWD)/.config/newsboat ~/.config/
+echo "Configure themes"
+rm -rf ~/.themes
+ln -sf "$DOTFILES_DIR/.themes" ~/.themes
 
-echo "Configure Nitrogen"
-rm -rf ~/.config/nitrogen
-ln -s $(echo $PWD)/.config/nitrogen ~/.config/
-
-echo "Configure nnn"
-rm -rf ~/.config/nnn
-ln -s $(echo $PWD)/.config/nnn ~/.config/
-
-echo "Configure k9s"
-rm -rf ~/.config/k9s
-ln -s $(echo $PWD)/.config/k9s ~/.config/
-
-echo "Configure cointop"
-rm -rf ~/.config/cointop
-ln -s $(echo $PWD)/.config/cointop ~/.config/
-
-echo "Configure dunst"
-rm -rf ~/.config/dunst
-ln -s $(echo $PWD)/.config/dunst ~/.config/
-
-echo "Configure zathura"
-rm -rf ~/.config/zathura
-ln -s $(echo $PWD)/.config/zathura ~/.config/
+echo "Done! Dotfiles configured successfully."
